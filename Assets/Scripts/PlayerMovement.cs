921bb38ee437a05e837770a5b1f2f56edee19193
RossRyoushi127 <reynado127@gmail.com>
2023-12-11 21:01:02 +0800

    Code Cleaning

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,14 +10,20 @@ public class PlayerMovement : MonoBehaviour
     float speed;
     void Start()
     {
+       
        _playerController = GetComponent<CharacterController>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-
-        _playerController.Move(move * Time.deltaTime * speed);    
+        MovementFucntion();
+   
     }
+    public void MovementFucntion()
+    {
+        Vector3 _move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        _playerController.Move(_move * Time.deltaTime * speed);
+    }
+
 }
