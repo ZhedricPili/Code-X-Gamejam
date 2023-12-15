e01a640904a21856963e2e8144b16f7fa004fbdd
ZhedricPauloB.Pili <zhedricpaulo@gmail.com>
2023-12-15 18:40:28 +0800

    feature/level-one:Added level scenes

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInventory : MonoBehaviour
 {
     //The variable can be read by other scripts but only be set here
     public int objectiveCounter {get; private set;}
+
+    public void Update()
+    {
+        if (objectiveCounter == 5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            objectiveCounter = 0;
+        }
+    }
 
     public void ObjectiveCollector()
     {
