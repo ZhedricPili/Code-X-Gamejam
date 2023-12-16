896ba5162191634e8c614c6e78b13f15ee4d3e09
RossRyoushi127 <reynado127@gmail.com>
2023-12-16 17:32:26 +0800

    player-movement-revamp:Revamped the player movement to have a third person camera.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,7 +8,13 @@ public class PlayerMovement : MonoBehaviour
 
     private CharacterController _playerController;
     [Tooltip("How fast player go brrrr")]
-    [SerializeField] float speed;
+    [SerializeField] 
+    public float speed;
+    public GameObject playerObject;
+    public Transform cam;
+    public float turnSmoothTime = 0.1f;
+
+    private float _turnSmoothVelocity;
 
     void Start()
     {   
@@ -22,8 +28,16 @@ public class PlayerMovement : MonoBehaviour
     }
     public void MovementFucntion()
     {
-        Vector3 _move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-        _playerController.Move(_move * Time.deltaTime * speed);
+        Vector3 _move = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical")).normalized;
+        if (_move.magnitude >= 0.1f)
+        {
+            float _targetAngle = Mathf.Atan2(_move.x,_move.z) * Mathf.Rad2Deg  + cam.eulerAngles.y;
+            float _angle = Mathf.SmoothDampAngle(playerObject.transform.eulerAngles.y,_targetAngle, ref _turnSmoothVelocity, turnSmoothTime);
+            playerObject.transform.rotation = Quaternion.Euler(0f, _angle, 0f);
+
+            Vector3 _moveDir = Quaternion.Euler(0f,_targetAngle,0f) * Vector3.forward;
+            _playerController.Move(_moveDir.normalized * Time.deltaTime * speed);   
+        }
     }
 
 }
